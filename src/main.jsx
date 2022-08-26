31a5b4931ee1a91c8ef224ee4b363370dca74e28
fedecanesa <federicocanesa@gmail.com>
2022-08-26 02:44:16 -0300

    Store and custom cursor is added

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './routes/App';
 import './index.scss';
-import { store } from './app/store'
-import { Provider } from 'react-redux'
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import { CustomCursor } from './components'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
+            <CustomCursor />
             <App />
         </Provider>
     </React.StrictMode>,
