956c845305d04c35b9dc9278d049bea1fde2a4b8
fedecanesa <federicocanesa@gmail.com>
2022-08-26 02:56:26 -0300

    Update the index file of the Components

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import CurrentWeather from './CurrentWeather/CurrentWeather';
+import CustomCursor from './CustomCursor/CustomCursor';
+import LocationForm from './LocationForm/LocationForm';
+import Layout from './Layout/Layout';
+import Loader from './Loader/Loader';
+import Menu from './Menu/Menu';
+import Title from './Title/Title';
+
+export {
+    CurrentWeather,
+    CustomCursor,
+    LocationForm,
+    Layout,
+    Loader,
+    Menu,
+    Title,
+};
