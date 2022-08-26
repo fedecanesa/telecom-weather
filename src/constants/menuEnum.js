9ef4d561f7ab8a5061665d4f87e886a4277da96e
fedecanesa <federicocanesa@gmail.com>
2022-08-26 02:54:47 -0300

    Add a menu enum

---
+++ src/constants/menuEnum.js
@@ -0,0 +1,22 @@
+export const menuEnum = [
+    {
+        id: 1,
+        title: 'London',
+    },
+    {
+        id: 2,
+        title: 'Sydney',
+    },
+    {
+        id: 3,
+        title: 'Tokyo',
+    },
+    {
+        id: 4,
+        title: 'Toronto',
+    },
+    {
+        id: 5,
+        title: 'Paris',
+    },
+];
