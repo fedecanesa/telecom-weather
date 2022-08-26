7b045e4b7ad84e487d65524c65840c9b30c60d6f
fedecanesa <federicocanesa@gmail.com>
2022-08-26 03:02:06 -0300

    Create basic Loader to work later

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './loader.scss'
+
+const Loader = () => {
+    return (
+        <div>Loader</div>
+    )
+}
+
+Loader.propTypes = {}
+
+export default Loader;
