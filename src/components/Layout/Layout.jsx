845519e4162b3c68c12201f4f0b3006057be8c2a
fedecanesa <federicocanesa@gmail.com>
2022-08-26 02:47:30 -0300

    General Layout is created.

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './layout.scss';
+
+const Layout = ({ children }) => {
+    return <div className='layout'>{children}</div>;
+};
+
+Layout.propTypes = {
+    children: PropTypes.object,
+};
+
+export default Layout;
