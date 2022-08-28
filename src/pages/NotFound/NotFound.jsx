21c1f9b445207b6bb3517153894eda03e165da8e
fedecanesa <federicocanesa@gmail.com>
2022-08-28 19:40:41 -0300

    Changing the UI of the not found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import { useNavigate } from "react-router-dom";
 import PropTypes from 'prop-types';
+import './notFound.scss';
 
-const NotFound = props => {
-    return <div>NotFound</div>;
-};
+const NotFound = () => {
+    const navigate = useNavigate();
+    const backHome = () => {
+        navigate('/')
+    }
+    return (
+        <>
+            <div className='notFound'>
+                <div className='notFound__text'>
+                    <h3>Oops!</h3>
+                    <h1>Your game hit a dead end</h1>
+                    <h2>Please go back to where you came from.</h2>
+                    <div className='buttonContainer'>
+                        <button onClick={backHome}>BACK TO HOMEPAGE</button>
+                    </div>
+                </div>
+                <div className='notFoun__img'>
+                    <img src="https://litebox.ai/img/404.bdfcf63c.svg" alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
 
-NotFound.propTypes = {};
+NotFound.propTypes = {
+    backHome: PropTypes.func
+}
 
-export default NotFound;
+export default NotFound
