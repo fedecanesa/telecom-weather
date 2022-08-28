20e49c82e373133c06eaee3fed26552f1220813c
fedecanesa <federicocanesa@gmail.com>
2022-08-28 19:36:00 -0300

    Create a Loader component

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,13 +1,62 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import { motion } from "framer-motion";
 import './loader.scss'
+
+const loadingContainerVariants = {
+    start: {
+        transition: {
+            staggerChildren: 0.2,
+        },
+    },
+    end: {
+        transition: {
+            staggerChildren: 0.2,
+        },
+    },
+};
+
+const loadingCircleVariants = {
+    start: {
+        y: "0%",
+    },
+    end: {
+        y: "60%",
+    },
+};
+const loadingCircleTransition = {
+    duration: 0.4,
+    yoyo: Infinity,
+    ease: 'easeInOut'
+}
 
 const Loader = () => {
     return (
-        <div>Loader</div>
+        <div className='loader'>
+            <div>
+                <motion.div
+                    className='loadingContainer'
+                    variants={loadingContainerVariants}
+                    initial="start"
+                    animate="end"
+                >
+                    <motion.span
+                        className='loadingCircle'
+                        variants={loadingCircleVariants}
+                        transition={loadingCircleTransition}
+                    ></motion.span>
+                    <motion.span
+                        className='loadingCircle'
+                        variants={loadingCircleVariants}
+                        transition={loadingCircleTransition}
+                    ></motion.span>
+                    <motion.span
+                        className='loadingCircle'
+                        variants={loadingCircleVariants}
+                        transition={loadingCircleTransition}
+                    ></motion.span>
+                </motion.div>
+            </div>
+        </div>
     )
 }
-
-Loader.propTypes = {}
 
 export default Loader;
