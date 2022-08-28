c7db9577577dc27631bb8b9108cd65cd5949d1ad
fedecanesa <federicocanesa@gmail.com>
2022-08-28 19:34:43 -0300

    Delete the custom cursor

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,10 @@ import App from './routes/App';
 import './index.scss';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import { CustomCursor } from './components'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
-            <CustomCursor />
             <App />
         </Provider>
     </React.StrictMode>,
