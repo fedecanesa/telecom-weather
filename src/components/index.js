f366645e5b37ebe00e2594ff0551e421122d5a95
fedecanesa <federicocanesa@gmail.com>
2022-08-29 00:18:06 -0300

    Delete the import of the CustomCursor

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,8 @@
 import CurrentWeather from './CurrentWeather/CurrentWeather';
-import CustomCursor from './CustomCursor/CustomCursor';
 import LocationForm from './LocationForm/LocationForm';
 import Layout from './Layout/Layout';
 import Loader from './Loader/Loader';
 import Menu from './Menu/Menu';
 import Title from './Title/Title';
 
-export {
-    CurrentWeather,
-    CustomCursor,
-    LocationForm,
-    Layout,
-    Loader,
-    Menu,
-    Title,
-};
+export { CurrentWeather, LocationForm, Layout, Loader, Menu, Title };
